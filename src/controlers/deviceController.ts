f416e06280796de918495bb01dd97ecff614031a
913-Goia-Dan <rares.goia@stud.ubbcluj.ro>
2024-03-30 16:44:51 +0200

    Added REST endpoints responsible for providing data for all entity-based features

--- src/controlers/deviceController.ts
+++ src/controlers/deviceController.ts
@@ -1,6 +1,60 @@
 import { Request, Response } from 'express';
 import { devices } from '../dataStore';
+import { Device } from '../models/device';
+
 
 export const getDevices = (req: Request, res: Response) => {
     res.json(devices);
 };
+
+export const getDeviceById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const device = devices.find(device => device.getId() === id);
+    if (device) {
+        res.json(device);
+    } else {
+        res.status(404).send('Device not found');
+    }
+};
+
+export const addDevice = async (req: Request, res: Response) => {
+    try {
+        const {id, name, price, image } = req.body;
+        const newDevice = new Device(
+            id,
+            name,
+            price,
+            image
+        );
+        devices.push(newDevice);
+        return res.status(201).json(newDevice);
+    } catch (error) {
+        console.error('Error adding device:', error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+}
+
+export const deleteDevice = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const index = devices.findIndex(device => device.getId() === id);
+    if (index > -1) {
+        devices.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Device not found');
+    }
+}
+
+export const updateDevice = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const device = devices.find(device => device.getId() === id);
+    if (device) {
+        const { name, price, image } = req.body;
+        device.setName(name);
+        device.setPrice(price);
+        device.setImage(image);
+        res.json(device);
+    } else {
+        res.status(404).send('Device not found');
+    }
+}
--- src/routes/deviceRoutes.ts
+++ src/routes/deviceRoutes.ts
@@ -1,9 +1,13 @@
 
 import express from 'express';
-import { getDevices } from '../controlers/deviceController';
+import { addDevice, deleteDevice, getDeviceById, getDevices, updateDevice } from '../controlers/deviceController';
 
 const router = express.Router();
 
 router.get('/devices', getDevices);
+router.get('/devices/:id', getDeviceById);
+router.post('/addDevice', addDevice);
+router.delete('/devices/:id', deleteDevice);
+router.put('/devices/:id', updateDevice);
 
 export default router;
