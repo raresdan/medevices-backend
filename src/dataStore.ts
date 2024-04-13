bb79c9b4b20ecc5413da21d2bab81cd7a97dd359
913-Goia-Dan <rares.goia@stud.ubbcluj.ro>
2024-04-13 21:31:50 +0300

    Data generation using Faker

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,21 +1,24 @@
-import { Device } from "./models/device";
+import { faker } from '@faker-js/faker';
+import Device from './models/device';
 
-let testDevice1: Device = new Device('X-Ray', 100, 'xray.jpg');
-let testDevice2: Device = new Device('Stethoscope', 20, 'stethoscope.jpg');
-let testDevice3: Device = new Device('Syringe', 5, 'syringe.jpg');
-let testDevice4: Device = new Device('X-Ray', 100, 'xray.jpg');
-let testDevice5: Device = new Device('Syringe', 7, 'syringe.jpg');
-let testDevice6: Device = new Device('Stethoscope', 25, 'stethoscope.jpg');
-let testDevice7: Device = new Device('X-Ray', 100, 'xray.jpg');
-let testDevice8: Device = new Device('X-Ray', 100, 'xray.jpg');
 
-export const devices: Device[] = [
-    testDevice1,
-    testDevice2,
-    testDevice3,
-    testDevice4,
-    testDevice5,
-    testDevice6,
-    testDevice7,
-    testDevice8,
+const medicalDeviceNames = [
+    'Heart Monitor',
+    'Blood Pressure Monitor',
+    'Pulse Oximeter',
+    'Ventilator',
+    'Defibrillator',
+    'Ultrasound Machine',
+    'MRI Machine',
+    'CT Scanner',
+    'X-Ray Machine',
+    'Dialysis Machine'
 ];
+
+export const devices: Device[] = Array.from({length: 20}, () => {
+    const name = medicalDeviceNames[faker.number.int({ min: 0, max: medicalDeviceNames.length - 1 })]
+    const price = parseFloat(faker.commerce.price());
+    const image = faker.image.url(); 
+
+    return new Device(name, price, image);
+});
